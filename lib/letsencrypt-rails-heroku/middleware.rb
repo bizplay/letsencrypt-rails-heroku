3e5c85b94cdaa8980cceee7e9f68202f227f9fad
Bob Groeneveld <bob.groeneveld@gmail.com>
2016-12-22 15:21:40 +0100

    removed the letsencrypt challenge response, the app will give that

--- lib/letsencrypt-rails-heroku/middleware.rb
+++ lib/letsencrypt-rails-heroku/middleware.rb
@@ -6,9 +6,9 @@ module Letsencrypt
     end
 
     def call(env)
-      if Letsencrypt.challenge_configured? && env["PATH_INFO"] == "/#{Letsencrypt.configuration.acme_challenge_filename}"
-        return [200, {"Content-Type" => "text/plain"}, [Letsencrypt.configuration.acme_challenge_file_content]]
-      end
+      # if Letsencrypt.challenge_configured? && env["PATH_INFO"] == "/#{Letsencrypt.configuration.acme_challenge_filename}"
+      #   return [200, {"Content-Type" => "text/plain"}, [Letsencrypt.configuration.acme_challenge_file_content]]
+      # end
 
       @app.call(env)
     end
